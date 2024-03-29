83257cdd02713b5d80d16d839ed5895f729aeef2
Andreas Scharf <andreas.b.scharf@gmail.com>
2024-03-29 18:40:48 +0100

    Prefinal

--- app/src/main/java/abs/apps/minimriddle/MainActivity.kt
+++ app/src/main/java/abs/apps/minimriddle/MainActivity.kt
@@ -1,34 +1,35 @@
 package abs.apps.minimriddle
 
+import abs.apps.minimriddle.ui.theme.MiniMRiddleTheme
 import android.os.Bundle
-import androidx.compose.foundation.Image
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import abs.apps.minimriddle.ui.theme.MiniMRiddleTheme
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -46,6 +47,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@Composable
+fun DigitOnlyTextField(value: String, onValueChange: (String) -> Unit) {
+    TextField(
+        value = value,
+        onValueChange = {
+            // Check if the new value consists only of digits before updating
+            if (it.all { char -> char.isDigit() }) {
+                onValueChange(it)
+            }
+        },
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+        label = { Text("Geheimer Zahlencode") }
+        // Add other parameters like Modifier as needed
+    )
+}
 
 @Composable
 fun NumberInputScreen(onPasswordCorrect: () -> Unit) {
@@ -70,7 +86,7 @@ fun NumberInputScreen(onPasswordCorrect: () -> Unit) {
             verticalArrangement = Arrangement.SpaceBetween // This will space the children evenly
         ) {
             Text(
-                text = "Du willst also unseren Schatz? Dann musst Du das Passwort kennen!",
+                text = "Noch hast Du unseren Schatz nicht, hahaha! Jetzt brauchst Du Zahlen.",
                 style = TextStyle(
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
@@ -80,7 +96,7 @@ fun NumberInputScreen(onPasswordCorrect: () -> Unit) {
             )
             Spacer(modifier = Modifier.weight(0.6f)) // This adds flexible space between text and input
             Text(
-                text = "Nur Taiwaner kennen das erste Geheimnis des Passworts",
+                text = "Nur ein Mannheimer kann Dir weiterhelfen.",
                 style = TextStyle(
                     fontWeight = FontWeight.Bold,
                     color = Color.White,
@@ -93,19 +109,15 @@ fun NumberInputScreen(onPasswordCorrect: () -> Unit) {
 
             // Keep the input field and button closer to each other
             Column {
-                TextField(
-                    value = password,
-                    onValueChange = { password = it },
-                    label = { Text("Geheimes Passwort eingeben") }
-                )
+                DigitOnlyTextField(value = password, onValueChange = { password = it })
                 Button(onClick = {
-                    if (password == "Oster-Pulk") {
+                    if (password == "10271802") {
                         onPasswordCorrect()
                     } else {
-                        // Handle incorrect password
+
                     }
                 }) {
-                    Text("Passwort abschicken!")
+                    Text("Zahlencode abschicken!")
                 }
 
             }
@@ -118,7 +130,13 @@ fun NumberInputScreen(onPasswordCorrect: () -> Unit) {
 @Composable
 fun Finale() {
     Column(modifier = Modifier.padding(16.dp)) {
-        Text(text = "Der Schatz wird von Emmie bewacht. Finde Emmie!")
+        Text(
+            text = "Der Schatz wird von Emmie bewacht. Finde Emmie!",
+            style = TextStyle(
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            ),
+        )
         Image(
             painter = painterResource(R.drawable.emmie),
             contentDescription = null, // Provide a description if needed
@@ -182,7 +200,7 @@ fun PasswordScreen(onPasswordCorrect: () -> Unit) {
                     label = { Text("Geheimes Passwort eingeben") }
                 )
                 Button(onClick = {
-                    if (password == "Oster-Pulk") {
+                    if (password == "Pulk") {
                         onPasswordCorrect()
                     } else {
                         // Handle incorrect password
